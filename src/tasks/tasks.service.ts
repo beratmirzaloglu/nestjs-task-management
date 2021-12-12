1a41a00cd79bd0f228ea9b8b5761b316b9e0f644
Berat Mirzaloglu <beratmirzaloglu@gmail.com>
2021-12-12 14:41:49 +0300

    get task, delete task, update task added

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -10,7 +11,13 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, description: string): Task {
+  getTaskById(id: string): Task {
+    const task = this.tasks.find((task) => task.id === id);
+    return task;
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
     const task: Task = {
       id: uuid(),
       title: title,
@@ -18,6 +25,17 @@ export class TasksService {
       status: TaskStatus.OPEN,
     };
     this.tasks.push(task);
+    console.log(this.tasks);
+    return task;
+  }
+
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  updateTasksStatus(id: string, newStatus: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = newStatus;
     return task;
   }
 }

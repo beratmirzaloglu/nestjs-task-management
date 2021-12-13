9dcad38845dd54c33b2b8be6eacb90a23c949bff
Berat Mirzaloğlu <berat.mirzaloglu@turkticaret.net>
2021-12-13 18:00:41 +0300

    pipes

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -39,6 +39,9 @@ export class TasksService {
 
   getTaskById(id: string): Task {
     const task = this.tasks.find((task) => task.id === id);
+    if (!task) {
+      throw new NotFoundException(`Task with ID: ${id} not found.`);
+    }
     return task;
   }
 
@@ -56,7 +59,9 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    const foundTask = this.getTaskById(id);
+
+    this.tasks = this.tasks.filter((task) => task.id !== foundTask.id);
   }
 
   updateTasksStatus(id: string, newStatus: TaskStatus): Task {

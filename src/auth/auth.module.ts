eec4c93b8e4c6615120db201ff80b4298d3c9cc4
Berat Mirzaloğlu <berat.mirzaloglu@turkticaret.net>
2021-12-16 14:20:58 +0300

    sign in and sign up added

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UsersRepository } from './users.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UsersRepository])],
+  providers: [AuthService],
+  controllers: [AuthController],
+})
+export class AuthModule {}

e54bc1fc234fffdc0c4ef8fe672132a9b3edab80
Berat Mirzaloğlu <berat.mirzaloglu@turkticaret.net>
2021-12-21 11:38:38 +0300

    import fixes

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -11,8 +11,8 @@ import {
   Logger,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { GetUser } from 'src/auth/get-user.decorator';
-import { User } from 'src/auth/user.entity';
+import { GetUser } from '../auth/get-user.decorator';
+import { User } from '../auth/user.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';

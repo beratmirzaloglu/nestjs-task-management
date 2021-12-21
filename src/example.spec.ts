75638d70893ad2f4f7097945cd73961bd1dd8ee3
Berat Mirzaloğlu <berat.mirzaloglu@turkticaret.net>
2021-12-21 11:29:55 +0300

    example test added

---
+++ src/example.spec.ts
@@ -0,0 +1,16 @@
+// function addNumbers(num1, num2) {
+//   return num1 + num2;
+// }
+
+// describe('Example test', () => {
+//   it('equals true', () => {
+//     expect(true).toEqual(true);
+//     expect('Berat').toEqual('Berat');
+//   });
+// });
+
+// describe('addNumbers', () => {
+//   it('adds two numbers', () => {
+//     expect(addNumbers(2, 2)).toEqual(4);
+//   });
+// });
